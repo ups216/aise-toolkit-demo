0cbfd1beee67e70bdbd08288a0ddbc7d463178fa
Lei Xu <leixu@leansoftx.com>
2024-05-03 00:06:25 +0800

    added streamlit app

--- aise_toolkit_demo/cli.py
+++ aise_toolkit_demo/cli.py
@@ -1,4 +1,7 @@
+import subprocess
 import click
+from click.testing import CliRunner
+import os
 
 @click.group()
 @click.version_option(version='aise, 0.1.1')
@@ -17,6 +20,22 @@ def cli():
 def first_command(example, option):
     "Command description goes here"
     click.echo("Here is some output")
+
+@cli.command(name="start_app")
+def start_app():
+    """Start the Streamlit app from ./app/app.py"""
+    # Path to your Streamlit script
+
+    # get the current directory
+    current_dir = os.getcwd()
+    click.echo(current_dir)
+
+    # Path to the Streamlit app
+    app_path = os.path.join(current_dir, "aise_toolkit_demo", "app", "app.py")
+    click.echo(app_path)
+
+    # Run the Streamlit app
+    subprocess.run(["streamlit", "run", app_path])
 
 if __name__ == "__main__":
     cli()
